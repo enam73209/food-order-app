3c5b6f2a0febe79f7db4b5eced12239d1ca74f35
enam73209 <enam.73209@gmail.com>
2024-08-20 21:50:33 +0600

    fetch data from api and loading and error handling

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,39 +1,65 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AvailableMeal.module.css";
 import MealCard from "./MealCard";
 import MealItem from "./MealItem";
 
 const AvailableMeals = () => {
-const[meals,setMeals]=useState([]);
-  useEffect(()=>{
-    const fetchMeals = async()=>{
-      const response =await (fetch('https://food-order-app-a0b24-default-rtdb.firebaseio.com/meals.json'));
-      const responseData = await response.json();
-      console.log(responseData);
-      const loadedMeals = [];
-      for(const key in responseData){
-        const meal = responseData[key];
-        if(meal){
-          loadedMeals.push({
-            id:key,
-            name: responseData[key].name,
-            description:responseData[key].description,
-            price:responseData[key].price
-          })
-        }
-        else{
-          console.warn(`Meal at index ${key} is null.`);
-        }
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-      }
-      setMeals(loadedMeals);
-      console.log(meals)
+  const fetchMeals = async () => {
+    const response = await fetch(
+      "https://food-order-app-a0b24-default-rtdb.firebaseio.com/meals.json"
+    );
+    if (!response.ok) {
+      // setIsLoading(false);
+      throw new Error("Something went wrong! Please try again later");
     }
-    fetchMeals();
-  },[])
-  const mealList = meals.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+    const responseData = await response.json();
+    console.log(responseData);
+    const loadedMeals = [];
+    for (const key in responseData) {
+      const meal = responseData[key];
+      if (meal) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      } else {
+        console.warn(`Meal at index ${key} is null.`);
+      }
+    }
+    setMeals(loadedMeals);
+    setIsLoading(false);
+    console.log(meals);
+  };
+  useEffect(() => {
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p className={classes.mealsLoading}>Loading...</p>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section>
+        <p className={classes.mealsError}>{error}</p>
+      </section>
+    );
+  }
+
+  const mealList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
 
   return (
     <section className={classes.meals}>
